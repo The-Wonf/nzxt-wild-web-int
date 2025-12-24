d7d955d00a18b91b6607e54d7453c3c3f03bdffe
Wonf <chevaliersacha62@gmail.com>
2025-12-24 16:58:55 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 
+const params = (new URL(window.location.href)).searchParams
+const source = 'gpu'
+
+window.document.getElementById('source').innerText = source
+
+function updateTemperature(temperature) {
+  window.document.getElementById('temperature').innerText = temperature
+}
+
+window.nzxt = {
+  v1: {
+    onMonitoringDataUpdate: (data) => {
+        const { cpus, gpus } = data
+        updateTemperature((source === 'cpu' ? cpus : gpus)[0].temperature)
+    }
+  }
+}
